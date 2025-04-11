08b7a357bb98df51eb38896be39e0c6101645f81
JitChowDhury <jitchowdhury8717@gmail.com>
2025-04-11 20:21:58 +0530

    add horizontal movement control

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,47 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public Animator animator;
+    public float Speed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        PlayMovementAnim(horizontalInput);
+        MoveCharacter(horizontalInput);
+    }
+
+    private void MoveCharacter(float horizontalInput)
+    {
+        Vector3 position = transform.position;
+        position.x += horizontalInput * Speed * Time.deltaTime;
+        transform.position = position;
+    }
+
+    private void PlayMovementAnim(float horizontalInput)
+    {
+        animator.SetFloat("Speed", Mathf.Abs(horizontalInput));
+
+        Vector3 scale = transform.localScale;
+        if (horizontalInput < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if (horizontalInput > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+
+        transform.localScale = scale;
     }
 }
